f5659811a6779e16ed2293838d96732cfde90cd3
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-05-23 15:54:47 +0200

    Initial Commit

---
+++ MensaBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MensaBar
+//
+//  Created by Max Niclas Wächtler on 23.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MensaBar/MensaBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MensaBarApp.swift
+//  MensaBar
+//
+//  Created by Max Niclas Wächtler on 23.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MensaBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
